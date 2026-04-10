21939c577b303d96d22e692b7c21d22e3277844a
Arun Busa <arun.busa131854@marwadiuniversity.ac.in>
2026-04-10 13:53:15 +0530

    fixed api base url with /api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://student-backend-e4fq.onrender.com"
+  baseURL: "https://student-backend-e4fq.onrender.com/api"
 });
 
 export default api;
